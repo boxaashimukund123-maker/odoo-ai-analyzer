921d7b4e714430565ea13498b506c0f91274cfca
Aashi <boxaashimukund123gmail.com>
2026-05-25 09:10:00 +0530

    fix ai insights page

--- app.py
+++ app.py
@@ -496,7 +496,7 @@ else:
     # AI INSIGHTS
     # =====================================
 
-if page == "AI Insights":
+ if "page" in locals() and page == "AI Insights":
 
     st.title("🧠 AI Business Insights")
 
