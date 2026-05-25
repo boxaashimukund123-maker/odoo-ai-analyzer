4ecebd185cf80abb928460ba7e9501f872e1dd0f
Aashi <boxaashimukund123gmail.com>
2026-05-25 10:02:02 +0530

    remove ai insights temporarily

--- app.py
+++ app.py
@@ -496,45 +496,7 @@ else:
     # AI INSIGHTS
     # =====================================
 
-if page == "AI Insights":
-
-    st.title("🧠 AI Business Insights")
-
-    st.info(
-        "AI predicts 21% sales growth next quarter."
-    )
-
-    st.subheader("📈 Revenue Growth")
-
-    st.line_chart(
-        [5, 8, 12, 18, 24, 32, 41]
-    )
-
-    st.subheader("📊 Sales Performance")
-
-    st.bar_chart(
-        [20, 14, 30, 25, 18]
-    )
-
-    st.subheader("⚡ System Performance")
-
-    st.write("AI Processing")
-    st.progress(92)
-
-    st.write("Server Stability")
-    st.progress(99)
-
-    st.write("Customer Satisfaction")
-    st.progress(87)
-
-    st.success(
-        "Customer engagement increased this week."
-    )
-
-    st.warning(
-        "Inventory for Product A may run low soon."
-    )
-
+    
     
     # =====================================
     # ODOO CONNECTION
